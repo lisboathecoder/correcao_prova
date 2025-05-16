5e47d2d28ccf86a046469c9791a3743c36dfd26f
Gustavo Lisboa <gustavo.lisboa2009@gmail.com>
2025-05-16 11:27:19 -0300

    commit

--- prova_02/exercicio_09.js
+++ prova_02/exercicio_09.js
@@ -4,10 +4,10 @@ let jogadores = ["Leo", "Sam", "Nina", "Gabi", "Caio"];
 let maisGols = gols[0];
 let artilheiro = jogadores[0];
 
-for(let i = 0; i < gols.length; i++) {
-    if(gols[i] > maisGols) {
-        maisGols = gols[i];
-        artilheiro = jogadores[i];
-    }
+for (let i = 0; i < gols.length; i++) {
+  if (gols[i] > maisGols) {
+    maisGols = gols[i];
+    artilheiro = jogadores[i];
+  }
 }
 console.log(`O artilheiro é ${artilheiro} com ${maisGols}!`);
